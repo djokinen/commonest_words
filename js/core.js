019c6c786d540d74f3ef24c1dc80d55e23480132
djokinen <djokinen@yahoo.com>
2017-09-21 00:29:41 -0400

    Add Mdl layout
    update speech tool + mdl

--- js/core.js
+++ js/core.js
@@ -1,8 +1,8 @@
 ﻿var words;
 var isSpeechSupported;
-var init = function () {
 
-	isSpeechSupported = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+var init = function () {
+	isSpeechSupported = responsiveVoice.voiceSupport();
 
 	initWords();
 
@@ -18,26 +18,25 @@ var init = function () {
 			initWords();
 			var msg = "Congratulations! You have read all " + words.length + " words";
 			if (isSpeechSupported) {
-				var talk = new SpeechSynthesisUtterance(msg);
-				window.speechSynthesis.speak(talk);
+				responsiveVoice.speak(msg);
 			}
 			alert(msg);
 		}
 	});
 
-	$("h1").click(function (e) {
+	$("#word,#listen").click(function (e) {
 		if (isSpeechSupported) {
-			var msg = new SpeechSynthesisUtterance($("#word").html());
-			window.speechSynthesis.speak(msg);
+			var msg = $("#word").html();
+			responsiveVoice.speak(msg);
 		}
 	});
 };
 
 var initWords = function () {
 	words = new Array("the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as",
-		"you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one",
-		"all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make",
-		"can", "like", "time", "no", "just", "him", "know", "take", "person", "into", "year", "your", "good", "some", "could", "them",
-		"see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two",
-		"how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us");
+	"you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one",
+	"all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make",
+	"can", "like", "time", "no", "just", "him", "know", "take", "person", "into", "year", "your", "good", "some", "could", "them",
+	"see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two",
+	"how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us");
 };
